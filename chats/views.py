72a0ff1a7b826e6469ed4b8291e043c15b46e96b
TMc42 <tmclean1212@gmail.com>
2022-04-07 14:44:52 -0500

    Rudimentary, ugly chat is now in place

--- chats/views.py
+++ chats/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
 from .models import Message,User
 
 def index(request):
     latest_messages = Message.objects.order_by('-timestamp')[:5]
-    output = ', '.join([m.message_content for m in latest_messages])
-    return HttpResponse(output)
+    context = {
+    'latest_messages': latest_messages,
+    }
+
+    return render(request, 'chats/index.html', context)
