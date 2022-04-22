42050d66e29ed7fe211ba841c4a851f285fb40af
TMc42 <tmclean1212@gmail.com>
2022-04-22 16:57:13 -0500

    Added input validation for blog posts

--- ddfuse.com/app/models/article.rb
+++ ddfuse.com/app/models/article.rb
@@ -1,2 +1,9 @@
 class Article < ApplicationRecord
+
+  # ==== INPUT VALIDATION ====
+  # Make sure the title is present
+  validates :title, presence: true
+  # Make sure the body is present and has a minimum length of 10
+  validates :body, presence: true, length: { minimum: 10}
+
 end
